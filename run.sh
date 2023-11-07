6e619607b92755a2b17217d9cdf656217754b96f
heehehe <heeseon0315@gmail.com>
2023-11-08 02:05:16 +0900

    feat: add upload_to_bigquery.py

--- run.sh
+++ run.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
 SHELL_PATH=$(dirname -- "${BASH_SOURCE[0]}")
+SCRIPT_PATH="${SHELL_PATH}/script"
 DATA_PATH="${SHELL_PATH}/data"
+
 mkdir -p "$DATA_PATH"
 
 SITE_LIST=("jumpit")
 
 
 for site_type in "${SITE_LIST[@]}"; do
-    python3 script/crawling.py \
+    python3 ${SCRIPT_PATH}/crawling.py \
        --site_type "$site_type" \
        --data_path "$DATA_PATH"
 
     cut -f3 "${DATA_PATH}/url.${site_type}.tsv" \
-        python3 script/tokenizing.py \
+        python3 ${SCRIPT_PATH}/tokenizing.py \
         > "${DATA_PATH}/tokens.${site_type}.tsv"
 
 done
+
+python3 ${SCRIPT_PATH}/upload_to_bigquery.py

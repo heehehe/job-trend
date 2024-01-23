9f03fed5b261cfa3f4c2ebc2b7a7fbb095a890fe
heehehe <heeseon0315@gmail.com>
2024-01-23 20:49:04 +0900

    feat: job category change to id

--- dbt_project/models/job_jumpit.sql
+++ dbt_project/models/job_jumpit.sql
@@ -4,5 +4,6 @@ WITH job_table AS (
     SELECT DISTINCT job_category, job_name
     FROM {{ source('crawling_data', 'jumpit') }}
 )
-SELECT CONCAT('jumpit_', job_category) AS job_category, job_name
+SELECT job_category, --CONCAT('jumpit_', job_category) AS job_category,
+       job_name
 FROM job_table
--- dbt_project/models/job_wanted.sql
+++ dbt_project/models/job_wanted.sql
@@ -4,5 +4,6 @@ WITH job_table AS (
     SELECT DISTINCT job_category, job_name
     FROM {{ source('crawling_data', 'wanted') }}
 )
-SELECT CONCAT('wanted_', job_category) AS job_category, job_name
+SELECT job_category, --CONCAT('wanted_', job_category) AS job_category,
+       job_name
 FROM job_table

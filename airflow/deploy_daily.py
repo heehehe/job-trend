24e56f346ace8a9d8bed9a388ddcd40d8d0d82f0
heehehe <heeseon0315@gmail.com>
2024-02-09 20:17:43 +0900

    feat: add run_dbt process

--- airflow/deploy_daily.py
+++ airflow/deploy_daily.py
@@ -2,21 +2,23 @@
 
 import os
 import sys
-from datetime import timedelta
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 
 
 DEFAULT_ARGS = {
-    'owner': 'DE4E',
+    'owner': 'admin',
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2024, 2, 10)
 }
 
-DIR_PATH = os.path.abspath(__file__)
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 SCRIPT_PATH = f"{DIR_PATH}/../script"
 DATA_PATH = f"{DIR_PATH}/../data"
+DBT_PROJECT_PATH = f"{DIR_PATH}/../dbt_project"
 
 if SCRIPT_PATH not in sys.path:
     sys.path.append(SCRIPT_PATH)
@@ -42,21 +44,28 @@ with DAG(
     for crawler_name, crawler in CRAWLING_CLASS.items():
         get_url_list[crawler_name] = PythonOperator(
             task_id=f"{crawler_name}.get_url_list",
-            python_callable=crawler.get_url_list()
+            python_callable=crawler.get_url_list
         )
         get_recruit_content_info[crawler_name] = PythonOperator(
             task_id=f"{crawler_name}.get_recruit_content_info",
-            python_callable=crawler.get_recruit_content_info()
+            python_callable=crawler.get_recruit_content_info
         )
         postprocess[crawler_name] = PythonOperator(
             task_id=f"{crawler_name}.postprocess",
-            python_callable=crawler.postprocess()
+            python_callable=crawler.postprocess
         )
 
-    upload_task = BashOperator(
+    upload_to_bigquery = BashOperator(
         task_id='upload_to_bigquery',
         bash_command=f'python3 {SCRIPT_PATH}/upload_to_bigquery.py'
     )
 
+    run_dbt = BashOperator(
+        task_id='run_dbt',
+        bash_command=f'cd {DBT_PROJECT_PATH}; dbt run'
+    )
+
     for crawler_name in CRAWLING_CLASS.keys():
-        get_url_list[crawler_name] >> get_recruit_content_info[crawler_name] >> postprocess[crawler_name] >> upload_task
+        get_url_list[crawler_name] >> get_recruit_content_info[crawler_name] >> postprocess[crawler_name] >> upload_to_bigquery
+
+    upload_to_bigquery >> run_dbt

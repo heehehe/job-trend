55705b311013e50065894fc22ba6bc5e755318fd
heehehe <heeseon0315@gmail.com>
2024-02-09 16:05:57 +0900

    feat: add dbt model files for jobplanet

---
+++ dbt_project/models/job/job_jobplanet.sql
@@ -0,0 +1,9 @@
+{{ config(materialized = 'view') }}
+
+WITH job_table AS (
+    SELECT DISTINCT job_category, job_name
+    FROM {{ source('crawling_data', 'jobplanet') }}
+)
+SELECT job_category, --CONCAT('wanted_', job_category) AS job_category,
+       job_name
+FROM job_table

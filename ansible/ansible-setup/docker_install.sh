e6e9fbf16c1e8d6d511bfcf52573a40de9c43de5
Yunho Song <dbsgh3322@gmail.com>
2024-04-28 22:18:19 +0900

    rm private code & update playbook

--- ansible/ansible-setup/docker_install.sh
+++ ansible/ansible-setup/docker_install.sh
@@ -12,5 +12,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# sudo usermod -aG docker songprem94 
 # newgrp docker
